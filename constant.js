0fa24ba212efe9109037ecfbac16044c7741bb11
TheerasakRung <147156330+TheerasakRung@users.noreply.github.com>
2025-05-25 21:36:30 +0700

    add env

--- constant.js
+++ constant.js
@@ -1,9 +1,3 @@
-const production = {
-  url: "https://erp-backend-5gd0.onrender.com",
-};
-const development = {
-  url: "http://localhost:5000",
-};
+require('dotenv').config()
 
-export const config =
-  import.meta.env.VITE_SITE_NAME === "dev" ? development : production;
+export const config = {url: process.env.BACKEND_URL}
